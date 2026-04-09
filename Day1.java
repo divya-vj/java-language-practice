18e206b83f11763d93fa431e537ca338be982126
Divya VJ <divyavj.8088@gmail.com>
2026-04-09 16:46:54 +0530

    Update Java practice file

--- Day1.java
+++ Day1.java
@@ -43,5 +43,64 @@ public class Day1 {
         String myText = "Hi"; // myText is a String
         // myText = 123;      // Error: cannot assign a number to a String
 
-    }
+        //float myNum = 5.75f;
+        System.out.println(myNum);
+
+        //double myNum = 19.99d;
+        System.out.println(myNum);
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println(f1);
+        System.out.println(d1);
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun);     // Outputs true
+        System.out.println(isFishTasty);   // Outputs false
+
+        char myGrade = 'B';
+        System.out.println(myGrade);
+        
+        char myVar1 = 65, myVar2 = 66, myVar3 = 67;
+        System.out.println(myVar1);
+        System.out.println(myVar2);
+        System.out.println(myVar3);
+
+        String greeting = "Hello World";
+        System.out.println(greeting);
+
+        int items = 50;
+        float costPerItem = 9.99f;
+        float totalCost = items * costPerItem;
+        char currency = '$';
+
+        // Print variables
+        System.out.println("Number of items: " + items);
+        System.out.println("Cost per item: " + costPerItem + currency);
+        System.out.println("Total cost = " + totalCost + currency);
+
+        var myNum = 5;         // int
+        var myDouble = 9.98;   // double
+        var myChar = 'D';      // char
+        var myBoolean = true;  // boolean
+        var myString = "Hello"; // String
+
+        //Type Casting
+
+        int myInt = 9;
+        double myDouble = myInt; // Automatic casting: int to double
+
+        System.out.println(myInt);    // Outputs 9
+        System.out.println(myDouble); // Outputs 9.0
+
+        double myDouble = 9.78d;
+        int myInt = (int) myDouble; // Manual casting: double to int
+
+        System.out.println(myDouble); // Outputs 9.78
+        System.out.println(myInt);    // Outputs 9
+
+        
+
+     }
 }
