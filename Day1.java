0e8fb13e8a961bf7511f0af6fc4ebe9918d5422b
Divya VJ <divyavj.8088@gmail.com>
2026-04-10 11:41:46 +0530

    Update Java practice file

--- Day1.java
+++ Day1.java
@@ -100,7 +100,15 @@ public class Day1 {
         System.out.println(myDouble); // Outputs 9.78
         System.out.println(myInt);    // Outputs 9
 
-        
+        //Operators
+
+        int sum1 = 100 + 50;        // 150 (100 + 50)
+        int sum2 = sum1 + 250;      // 400 (150 + 250)
+        int sum3 = sum2 + sum2;     // 800 (400 + 400)
+
+
+
+
 
      }
 }
