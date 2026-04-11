5d9db8c3c81f273ec32a9bc8fe52911f57c83d22
Divya VJ <divyavj.8088@gmail.com>
2026-04-11 18:52:27 +0530

    Add Java practice file

---
+++ Day2.java
@@ -0,0 +1,15 @@
+public class Day2 {
+    public static void main(String[] args) {
+        Math.max(5, 10);
+        Math.min(5, 10);
+        Math.sqrt(64);
+        Math.abs(-4.7);
+        Math.pow(2, 8);  // 256.0
+        Math.round(4.6);  // 5
+        Math.ceil(4.1);   // 5.0
+        Math.floor(4.9);  // 4.0
+
+
+
+    }
+}
