bb76b14ffdac657c63ee23d72816d36126bb5d99
Divya VJ <divyavj.8088@gmail.com>
2026-04-13 22:22:23 +0530

    Update Java practice file

--- Day3.java
+++ Day3.java
@@ -96,6 +96,26 @@ public class Day3 {
         }
         System.out.println("Sum is " + sum);
 
+        // Outer loop
+        for (int i = 1; i <= 2; i++) {
+        System.out.println("Outer: " + i); // Executes 2 times
+        
+        // Inner loop
+        for (int j = 1; j <= 3; j++) {
+            System.out.println(" Inner: " + j); // Executes 6 times (2 * 3)
+        }
+        } 
+
+        for (int i = 1; i <= 3; i++) {
+            
+            for (int j = 1; j <= 3; j++) {
+            System.out.print(i * j + " ");
+        }
+        System.out.println();
+        }
+
+
+
 
 
 
