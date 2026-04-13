bd41edfb4350bd5414515bcfd1b5e532c9a88550
Divya VJ <divyavj.8088@gmail.com>
2026-04-13 22:17:39 +0530

    Update Java practice file

--- Day3.java
+++ Day3.java
@@ -84,6 +84,20 @@ public class Day3 {
         dice = dice + 1;
         }
 
+        //for loop
+
+        for (int i = 0; i < 5; i++) {
+        System.out.println(i);
+        }
+
+        int sum = 0;
+        for (int i = 1; i <= 5; i++) {
+        sum = sum + i;
+        }
+        System.out.println("Sum is " + sum);
+
+
+
 
 
 
