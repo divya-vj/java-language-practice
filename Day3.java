a86216b4b568ffec151d03cf08894752558dc7d8
Divya VJ <divyavj.8088@gmail.com>
2026-04-13 22:28:13 +0530

    Update Java practice file

--- Day3.java
+++ Day3.java
@@ -125,6 +125,16 @@ public class Day3 {
         System.out.println(num);
         }
 
+        int n = 5;
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+        fact *= i;
+        }
+
+        System.out.println("Factorial of " + n + " is " + fact);
+        // Output: Factorial of 5 is 120
+
         
 
 
