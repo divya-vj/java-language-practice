84943009388a9769af38aaa0449a26f817cb8767
Divya VJ <divyavj.8088@gmail.com>
2026-04-15 18:59:50 +0530

    Update Java practice file

--- Day4.java
+++ Day4.java
@@ -112,6 +112,18 @@ public class Day4 {
                 }
 
                 //scope
+                public class Main {
+                public static void main(String[] args) {
+
+                    for (int i = 0; i < 3; i++) {
+                    System.out.println("Loop 1: " + i);
+                    }
+
+                    for (int i = 0; i < 2; i++) {
+                    System.out.println("Loop 2: " + i);
+                    }
+                }
+                }
 
 
 
