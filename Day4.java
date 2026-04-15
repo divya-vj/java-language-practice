25ecfe3c8f9a0d48cec74f1589475f280d314438
Divya VJ <divyavj.8088@gmail.com>
2026-04-15 19:11:06 +0530

    Update Java practice file

--- Day4.java
+++ Day4.java
@@ -125,7 +125,62 @@ public class Day4 {
                 }
                 }
 
+                //recursion
 
+                public class Main {
+                public static int sum(int k) {
+                    if (k > 0) {
+                    return k + sum(k - 1);
+                    } else {
+                    return 0;
+                    }
+                }
+
+                public static void main(String[] args) {
+                    int result = sum(10);
+                    System.out.println(result);
+                }
+                }
+
+                public class Main {
+                public static int sum(int start, int end) {
+                    if (end > start) {
+                    return end + sum(start, end - 1);
+                    } else {
+                    return end;
+                    }
+                }
+
+                public static void main(String[] args) {
+                    int result = sum(5, 10);
+                    System.out.println(result);
+                }
+                }
+                public class Main {
+                static void countdown(int n) {
+                    if (n > 0) {
+                    System.out.print(n + " ");
+                    countdown(n - 1);
+                    }
+                }
+
+                public static void main(String[] args) {
+                    countdown(5);
+                }
+                }
+                public class Main {
+                static int factorial(int n) {
+                    if (n > 1) {
+                    return n * factorial(n - 1);
+                    } else {
+                    return 1;
+                    }
+                }
+
+                public static void main(String[] args) {
+                    System.out.println("Factorial of 5 is " + factorial(5));
+                }
+                }
 
 
 
