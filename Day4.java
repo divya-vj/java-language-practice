dd83498f0779917b5c3c80e666ab0d6c3953c488
Divya VJ <divyavj.8088@gmail.com>
2026-04-15 18:12:01 +0530

    Add Java practice file

---
+++ Day4.java
@@ -0,0 +1,23 @@
+public class Day4 {
+    public static void main(String args[]){
+        //method
+
+        public class Main {
+            static void myMethod() {
+                System.out.println("I just got executed!");
+            }
+
+            public static void main(String[] args) {
+                myMethod();
+            }
+    }
+
+            // Outputs "I just got executed!"
+
+    
+ 
+
+
+    }
+    
+}
