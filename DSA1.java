8b309ab751ea022c6315adf572d8a700dc06bebe
Divya VJ <divyavj.8088@gmail.com>
2026-05-01 11:25:33 +0530

    Update Java practice file

--- DSA1.java
+++ DSA1.java
@@ -27,4 +27,37 @@ public class LinearSearch {
     }
 }
 
+import java.util.Scanner;
 
+public class MaxMin {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // take array input
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // assume first element is both max and min
+        int max = arr[0];
+        int min = arr[0];
+
+        // start loop from index 1 (already checked index 0)
+        for(int i = 1; i < n; i++) {
+
+            if(arr[i] > max) {
+                max = arr[i]; // found new maximum
+            }
+
+            if(arr[i] < min) {
+                min = arr[i]; // found new minimum
+            }
+        }
+
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+    }
+}
