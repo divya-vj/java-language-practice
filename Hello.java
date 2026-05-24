0948ca84fe2506f3468bcf31d72d83962d8f1454
Divya VJ <divyavj.8088@gmail.com>
2026-05-24 15:32:58 +0530

    Update Java practice file

--- Hello.java
+++ Hello.java
@@ -1,24 +1,33 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        System.out.println("Enter a string:");
-        String s = sc.nextLine();
+        System.out.println("Enter first string:");
+        String s1 = sc.nextLine().toLowerCase();
         
-        // Convert to lowercase so "Madam" and "madam" both work
-        s = s.toLowerCase();
+        System.out.println("Enter second string:");
+        String s2 = sc.nextLine().toLowerCase();
         
-        String reversed = "";
-        for (int i = s.length() - 1; i >= 0; i--) {
-            reversed = reversed + s.charAt(i);
+        // If lengths differ they cannot be anagrams
+        if (s1.length() != s2.length()) {
+            System.out.println("NOT Anagrams");
+            return;
         }
         
-        if (s.equals(reversed)) {
-            System.out.println(s + " is a Palindrome");
+        // Convert to char array, sort, compare
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        
+        if (Arrays.equals(arr1, arr2)) {
+            System.out.println("Anagrams");
         } else {
-            System.out.println(s + " is NOT a Palindrome");
+            System.out.println("NOT Anagrams");
         }
     }
 }
