b99ae6e9f92059fa4aeae61613b3a5be20faa384
Divya VJ <divyavj.8088@gmail.com>
2026-05-24 15:23:27 +0530

    Update Java practice file

--- Hello.java
+++ Hello.java
@@ -1,26 +1,24 @@
-String s="divya";
-
-for(int i=0;i<s.length;i++){
-    char c=s.charAt(i);
-    System.out.println(c);
-}
-
-//string reversal
-
 import java.util.Scanner;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter a string:");
-        String s=sc.nextLine();
-        String reversed="";
-
-        for (int i=s.length()-1;i>=0;i--){
-            reversed = reversed+s.charAt(i);
-
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter a string:");
+        String s = sc.nextLine();
+        
+        // Convert to lowercase so "Madam" and "madam" both work
+        s = s.toLowerCase();
+        
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--) {
+            reversed = reversed + s.charAt(i);
         }
-        System.out.println("reversed" + reversed)
-    }}
+        
+        if (s.equals(reversed)) {
+            System.out.println(s + " is a Palindrome");
+        } else {
+            System.out.println(s + " is NOT a Palindrome");
+        }
+    }
 }
-
